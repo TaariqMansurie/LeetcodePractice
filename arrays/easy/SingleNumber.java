c79d26784de858c36baab90b2f5baf835974e641
Mohammed Taariq Amin Mansurie <mansurie@pdx.edu>
2025-01-06 18:49:11 -0800

    added Single Number java file

---
+++ arrays/easy/SingleNumber.java
@@ -0,0 +1,28 @@
+package arrays.easy;
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int result = 0; //since XOR with 0 returns same number
+//         for(int i=0; i<nums.length; i++) {
+//             result = result^nums[i];        // result = (result) XOR (array element at i) 
+//         }
+//         return result; 
+//     }
+// }
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; // XOR each number with the result
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.singleNumber(new int[]{2, 2, 1})); // Output: 1
+        System.out.println(solution.singleNumber(new int[]{4, 1, 2, 1, 2})); // Output: 4
+        System.out.println(solution.singleNumber(new int[]{1})); // Output: 1
+    }
+}
