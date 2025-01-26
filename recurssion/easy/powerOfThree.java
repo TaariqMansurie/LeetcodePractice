d1b3775f21a0b7aac0df81c208d97f601e01da7f
Mohammed Taariq Amin Mansurie <mansurie@pdx.edu>
2025-01-26 15:34:14 -0800

    added file for power of three

---
+++ recurssion/easy/powerOfThree.java
@@ -0,0 +1,23 @@
+package recurssion.easy;
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+      //base case
+        // if (n%2 != 0 ){
+        //     return ;
+        // }
+        if (n <= 0) return false; // Negative numbers and zero are not powers of three.
+        if (n == 1) return true;  // 3^0 = 1, so 1 is a power of two.
+        //kaam
+        // isPoweOfThree(3^x=n);
+        return (n % 3 == 0) && isPowerOfThree(n / 3);
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        int n = 3;
+        System.out.println(sol.isPowerOfThree(n));
+
+        int n2 = 4; // Example input
+        System.out.println(sol.isPowerOfThree(n2)); // Output: false
+    }
+}
