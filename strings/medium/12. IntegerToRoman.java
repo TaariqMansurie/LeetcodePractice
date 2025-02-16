f4d61a1310829ce17da29e8a5cfa9f03a96f60eb
Mohammed Taariq Amin Mansurie <mansurie@pdx.edu>
2025-02-15 23:48:26 -0800

    added java file of IntegerToRoman.java

---
+++ strings/medium/12. IntegerToRoman.java
@@ -0,0 +1,28 @@
+package strings.medium;
+
+public class Solution {
+    public String intToRoman(int num) {
+        // Define the values and their corresponding Roman numerals
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder result = new StringBuilder();
+
+        // Iterate through the values and construct the Roman numeral
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result.append(symbols[i]);
+            }
+        }
+        
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.intToRoman(3749)); // Output: "MMMDCCXLIX"
+        System.out.println(sol.intToRoman(58));   // Output: "LVIII"
+        System.out.println(sol.intToRoman(1994)); // Output: "MCMXCIV"
+    }
+}
